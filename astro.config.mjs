9215d1d42c9768d3c59e603bfbbde39aab55b88f
Diógenes Rodrigues <dxdns>
2025-04-29 19:16:25 -0300

    first

---
+++ astro.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Svelte Components",
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/dxdns/svelte-components",
+        },
+      ],
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", slug: "guides/example" },
+            { label: "test", slug: "guides/test" },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+    svelte(),
+  ],
+});
