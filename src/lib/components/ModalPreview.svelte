bf85eb820beffbfabad7b952ef0bcf6ffedd005c
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-20 11:50:31 -0300

    modal component

---
+++ src/lib/components/ModalPreview.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { Button, Modal } from "@dxdns/feflow"
+
+	let isOpen = $state(false)
+
+	function handleClose() {
+		isOpen = false
+	}
+</script>
+
+<Modal {isOpen} {handleClose}>
+	<Modal.Header {handleClose} align="center">
+		<h2>test header</h2>
+	</Modal.Header>
+	<Modal.Content>
+		<p>test content</p>
+	</Modal.Content>
+	<Modal.Actions class="border-0" align="center" style="align-items: baseline;">
+		<Button variant="outlined" onclick={handleClose}>Cancel</Button>
+		<Button>Save</Button>
+	</Modal.Actions>
+</Modal>
+
+<Button
+	onclick={() => {
+		isOpen = !isOpen
+	}}
+>
+	Open Modal
+</Button>
