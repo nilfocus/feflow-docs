03b8fd8a83448558b9c95a9aa5ff3c3690e7744b
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-20 10:11:24 -0300

    code component

---
+++ src/lib/components/CodePreview.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Code } from "@dxdns/feflow"
+
+	const data = `
+        .card {
+            width: 400px;
+            padding: 20px;
+            border: 1px solid #0d1117;
+            border-radius: 10px;
+            background-color: #000;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+            cursor: pointer;
+        }`
+</script>
+
+<Code>
+	{#each data.split("\n").filter((l) => l.trim() !== "") as text, i}
+		<pre data-prefix={i}>
+			<code>{text.trim()}</code>
+		</pre>
+	{/each}
+</Code>
