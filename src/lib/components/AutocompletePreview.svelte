f4a5100a44ad3bd99820516f53be07dffb98a57e
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-20 09:19:08 -0300

    autocomplete component

---
+++ src/lib/components/AutocompletePreview.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { Autocomplete } from "@dxdns/feflow"
+
+	const data = Array.from(Array(30)).map((_, i) => ({
+		id: i,
+		label: `Item ${i}`
+	}))
+</script>
+
+<Autocomplete variant="search" {data} filter={(item) => `${item.label}`}>
+	{#snippet renderInput({ id, label })}
+		<a href="/{id}">
+			{label}
+		</a>
+	{/snippet}
+</Autocomplete>
