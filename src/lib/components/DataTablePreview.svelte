eef9f49b0905c4d4de61d1700832d0a24d0f2a17
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-20 10:25:34 -0300

    data table component

---
+++ src/lib/components/DataTablePreview.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { DataTable } from "@dxdns/feflow"
+
+	const data = Array.from(Array(10)).map((_, index) => ({
+		id: index,
+		label: `test-${index}`
+	}))
+</script>
+
+<DataTable {data} />
