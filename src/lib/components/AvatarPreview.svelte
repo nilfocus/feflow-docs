58b2a4c15a29fc6dc02efc4a5c89c7af901e629d
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-20 09:32:56 -0300

    avatar component

---
+++ src/lib/components/AvatarPreview.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Avatar } from "@dxdns/feflow"
+</script>
+
+<Avatar textFallback="test" />
+<br />
+<Avatar
+	src="https://img.odcdn.com.br/cdn-cgi/image/width=1200,height=1200,fit=cover/wp-content/uploads/2023/01/Avatar-Loak.jpg"
+	textFallback="test"
+/>
+<br />
+<Avatar
+	src="https://avatars.githubusercontent.com/u/26321303?v=4"
+	textFallback="test"
+	width="150px"
+	height="150px"
+/>
