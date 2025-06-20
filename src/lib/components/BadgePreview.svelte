e337b727ab401f2585a664b30d96be6b0e6bdb61
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-20 09:47:06 -0300

    badge component

---
+++ src/lib/components/BadgePreview.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { Badge } from "@dxdns/feflow"
+
+	const variants = ["outlined", "text", "contained"]
+	const colors = ["error", "success", "warning", "info", "primary"]
+	const sizes = ["xs", "sm", "md", "lg", "xl"]
+</script>
+
+<div
+	style="
+    display: flex; 
+    flex-direction: column; 
+    align-items: center; 
+    gap: 1rem;
+    "
+>
+	{#each colors as color}
+		<Badge class="bg-{color} text-on-{color}" size="md">
+			{color}
+		</Badge>
+	{/each}
+	{#each variants as variant}
+		<Badge variant={variant as any} size="md">
+			{variant}
+		</Badge>
+	{/each}
+	{#each sizes as size}
+		<Badge roundedFull size={size as any}>
+			{size}
+		</Badge>
+	{/each}
+</div>
