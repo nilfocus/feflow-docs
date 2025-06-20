932054c4eee702d67c0e3b2daf994045250f247c
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-20 11:17:58 -0300

    kbd component

---
+++ src/lib/components/KbdPreview.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Kbd } from "@dxdns/feflow"
+
+	const sizes = ["xs", "sm", "md", "lg", "xl"]
+</script>
+
+{#each sizes as size}
+	<div>
+		<Kbd size={size as any}>Ctrl</Kbd>
+		<Kbd size={size as any}>K</Kbd>
+	</div>
+	<br />
+	<br />
+{/each}
