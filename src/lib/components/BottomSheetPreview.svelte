861e8a42c55bb7611af7c69dd4b0dc1011f2f80d
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-20 09:57:44 -0300

    bottom sheet component

---
+++ src/lib/components/BottomSheetPreview.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { BottomSheet, Button } from "@dxdns/feflow"
+
+	let isOpen = $state(false)
+</script>
+
+<BottomSheet
+	{isOpen}
+	handleClose={() => {
+		isOpen = false
+	}}
+>
+	<h1>test</h1>
+</BottomSheet>
+
+<Button
+	onclick={() => {
+		isOpen = !isOpen
+	}}
+>
+	Open
+</Button>
