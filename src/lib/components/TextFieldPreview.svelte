7c91cb7b638f6635319768ecd184679b20c3bc13
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-24 11:06:37 -0300

    text field

---
+++ src/lib/components/TextFieldPreview.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { TextField } from "@dxdns/feflow"
+
+	const sizes = ["xs", "sm", "md", "lg", "xl"]
+	const variants = ["contained", "text", "outlined"]
+</script>
+
+<div style="max-width: 300px; margin: 0 auto;">
+	{#each variants as variant}
+		<TextField label={variant} variant={variant as any} />
+		<br />
+		<br />
+	{/each}
+
+	{#each sizes as size, i}
+		<TextField
+			label={size}
+			variant={i % 2 ? "contained" : "text"}
+			size={size as any}
+		/>
+		<br />
+		<br />
+	{/each}
+</div>
