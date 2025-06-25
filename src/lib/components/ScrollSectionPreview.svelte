2ee5ede76f4eea996126103d3f6a952999277000
nilfocus <217644694+nilfocus@users.noreply.github.com>
2025-06-25 11:54:05 -0300

    guide scroll section

---
+++ src/lib/components/ScrollSectionPreview.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { getSections, ScrollSection, Window } from "@dxdns/feflow"
+
+	const sections = $derived(getSections())
+	const sectionsData = Array.from({ length: 25 }, (_, index) => ({
+		reference: `Section ${index + 1}`
+	}))
+</script>
+
+<Window style="max-height: 300px; overflow-y: scroll;">
+	<ScrollSection
+		data={sections}
+		style="
+        position: sticky; 
+        margin: 0; 
+        top: -16px; 
+        z-index: 1;
+        "
+	>
+		{#each sectionsData as { reference }}
+			<ScrollSection.Content
+				id={reference}
+				style="
+                height: 100vh; 
+                border: 1px solid red; 
+                padding: 1rem;
+                "
+			>
+				<span>{reference}</span>
+			</ScrollSection.Content>
+		{/each}
+	</ScrollSection>
+</Window>
