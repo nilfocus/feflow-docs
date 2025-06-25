08f135afcc9ff3d350cc3fb820f2e393a08d46bc
nilfocus <217644694+nilfocus@users.noreply.github.com>
2025-06-25 17:22:24 -0300

    docs: shortcut listener

---
+++ src/lib/components/ShortcutListenerPreview.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { Kbd, ShortcutListener } from "@dxdns/feflow"
+
+	let message = $state("waiting...")
+
+	function handleShortcut(e: KeyboardEvent) {
+		const keys = []
+		if (e.ctrlKey) {
+			keys.push("ctrl")
+			keys.push("+")
+		}
+		keys.push(e.key)
+		message = `Shortcut ${keys.join(" ")} pressed!`
+	}
+</script>
+
+<h4>{message}</h4>
+
+<ShortcutListener keys={["Control", "C"]} callback={handleShortcut}>
+	<p>Press <Kbd>Ctrl</Kbd> + <Kbd>C</Kbd></p>
+    
+</ShortcutListener>
+<p>or</p>
+<ShortcutListener keys={["Control", "V"]} callback={handleShortcut}>
+	<p>Press <Kbd>Ctrl</Kbd> + <Kbd>V</Kbd></p>
+</ShortcutListener>
