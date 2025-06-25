17059a3ab8acaba873477dd6baf00a20058a1f00
nilfocus <217644694+nilfocus@users.noreply.github.com>
2025-06-25 10:28:44 -0300

    guide pin input

---
+++ src/lib/components/PinInputPreview.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+	import { Button, Modal, PinInput } from "@dxdns/feflow"
+	import { onMount } from "svelte"
+
+	let isOpen = $state(false)
+
+	function handleOpen() {
+		isOpen = !isOpen
+	}
+
+	function handleClose() {
+		isOpen = false
+	}
+
+	onMount(() => {
+		isOpen = true
+	})
+</script>
+
+<Modal {isOpen} {handleClose}>
+	<Modal.Header align="center">
+		<h3>Two-Factor Verification</h3>
+	</Modal.Header>
+	<Modal.Content
+		style="
+        max-width: 500px; 
+        margin: 0 auto; 
+        text-align: center;
+        "
+	>
+		<p>
+			Enter the two-factor authentication code provided by the authenticator app
+		</p>
+		<br />
+		<div
+			style="
+            display: flex; 
+            align-items: center; 
+            justify-content: center;
+            overflow: hidden;
+            flex-wrap: nowrap;
+            gap: 0.5rem;
+            "
+		>
+			{#each Array.from(Array(4)) as _, i}
+				<PinInput isLoading={i === 0} />
+			{/each}
+		</div>
+	</Modal.Content>
+	<Modal.Actions class="border-0" align="center">
+		<Button
+			class="hover-border-on-error hover-text-on-error"
+			variant="outlined"
+			onclick={handleClose}
+		>
+			Cancel
+		</Button>
+		<Button onclick={handleClose}>Save</Button>
+	</Modal.Actions>
+</Modal>
+
+<Button onclick={handleOpen}>open</Button>
