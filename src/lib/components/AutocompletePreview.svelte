b4fa747b73ccfc2ab0ee903f94101d6384c83ace
dxdns <37380674+dxdns@users.noreply.github.com>
2025-06-26 20:26:00 -0300

    autocomplete variants

--- src/lib/components/AutocompletePreview.svelte
+++ src/lib/components/AutocompletePreview.svelte
@@ -3,14 +3,23 @@
 
 	const data = Array.from(Array(30)).map((_, i) => ({
 		id: i,
-		label: `Item ${i}`
+		label: `test ${i}`
 	}))
+
+	const variants = ["text", "contained", "outlined"]
 </script>
 
-<Autocomplete variant="search" {data} filter={(item) => `${item.label}`}>
-	{#snippet renderInput({ id, label })}
-		<Link href="/{id}">
-			{label}
-		</Link>
-	{/snippet}
-</Autocomplete>
+{#each variants as variant}
+	<Autocomplete
+		variant={variant as any}
+		{data}
+		filter={(item) => `${item.label}`}
+		placeholder="test"
+	>
+		{#snippet renderInput({ id, label })}
+			<Link href="/{id}">
+				{label}
+			</Link>
+		{/snippet}
+	</Autocomplete>
+{/each}
