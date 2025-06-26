cdd61487ab4f7a678159829d177e90e0d764859f
nilfocus <217644694+nilfocus@users.noreply.github.com>
2025-06-26 11:45:35 -0300

    docs: status line break

--- src/lib/components/StatusPreview.svelte
+++ src/lib/components/StatusPreview.svelte
@@ -8,6 +8,7 @@
 <div style="max-width: 300px; margin: 0 auto; line-height: 3;">
 	{#each variants as variant, i}
 		<Status variant={variant as any} color={colors[i]} />
+		<br />
 	{/each}
 
 	{#each ["5rem", 100, "150px", 200] as size, i}
@@ -16,5 +17,6 @@
 			variant={i % 2 ? "bounce" : "none"}
 			{size}
 		/>
+		<br />
 	{/each}
 </div>
