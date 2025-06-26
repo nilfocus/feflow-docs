7746e400463e12b0de8603b8426b3f8c9eaec45c
nilfocus <217644694+nilfocus@users.noreply.github.com>
2025-06-26 13:07:56 -0300

    docs: status theme color style

--- src/lib/components/StatusPreview.svelte
+++ src/lib/components/StatusPreview.svelte
@@ -1,19 +1,22 @@
 <script lang="ts">
-	import { Status } from "@dxdns/feflow"
+	import { Status, themeConfig } from "@dxdns/feflow"
 
+	const { colors } = $derived(themeConfig())
 	const variants = ["pulse", "ping", "bounce"]
-	const colors = ["blue", "red", "green"]
 </script>
 
 <div style="max-width: 300px; margin: 0 auto; line-height: 3;">
 	{#each variants as variant, i}
-		<Status variant={variant as any} color={colors[i]} />
+		<Status
+			variant={variant as any}
+			color={i % 2 ? colors.colorSuccess : colors.colorError}
+		/>
 		<br />
 	{/each}
 
 	{#each ["5rem", 100, "150px", 200] as size, i}
 		<Status
-			color={i % 2 ? "aqua" : "orange"}
+			color={i % 2 ? colors.colorInfo : colors.colorWarning}
 			variant={i % 2 ? "bounce" : "none"}
 			{size}
 		/>
